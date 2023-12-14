c0ff29f690b05dccc4b289923f0edb4ddcf18f06
turan-98 <santanasamuel08@gmail.com>
2023-12-14 12:59:55 -0300

    🎨

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const helmet = require('helmet');
 const middleware = require('./src/middlewares/middleware');
+const verifyCsrf = require('./src/middlewares/middleware');
+const verifyToken = require('./src/middlewares/middleware');
+const csrfMiddleWare = require('./src/middlewares/middleware');
+const csrf = require('csurf');
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.CONNECTIOSTRING)
 .then(() => {
   //retorna um sinal de quando conectou 
   app.emit('done');
 })
 
-app.use(middleware)
+app.use(helmet())
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const session = require('express-session');
 const mongoStore = require('connect-mongo')
 const flashMsg = require('connect-flash');
+
 
 app.use(
   session({
@@ -35,9 +41,18 @@ app.use(
 
 app.use(flashMsg());
 
-
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
+
+app.use(csrf())
+app.use(function(req,res,next){
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+app.use(middleware)
+app.use(verifyCsrf)
+app.use(verifyToken)
+
 
 app.use(routes);
 
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,3 +1,13 @@
 module.exports = (req,res, next) => {
     next();
 }
+
+exports.verifyCsrf = (err, req,res,next) => {
+    if(err && 'EBADCSRFTOKEN' === err.code  )
+        return res.render('404')
+}
+
+exports.verifyToken = (req, res, next) => {
+    res.locals.crsfToken = req.crsfToken();
+    next();
+}
